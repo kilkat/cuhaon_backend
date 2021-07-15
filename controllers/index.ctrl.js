b294e9a874b406b9777a5463aa20aba718d56904
MUNSY-eggrice <dnwnskfk333@gmail.com>
2021-07-15 21:13:03 +0900

    0715 문영민

--- controllers/index.ctrl.js
+++ controllers/index.ctrl.js
@@ -1,23 +1,46 @@
-const Wargame = require("../schemas/wargame");
-const Comment = require("../schemas/comment");
-const User = require("../schemas/user");
-
+const Wargame = require('../schemas/wargame');
+const Comment = require('../schemas/comment');
+const User = require('../schemas/user');
+//메인페이지
 const getIndexPage = (req, res) => {
-  res.render("index");
+  res.render('index');
 };
-
+//wargame 페이지
 const getWargameIndexPage = async (req, res) => {
   try {
-    const wargame = await Wargame.find();
+    //페이징 구문
+    let page = Math.max(1, parseInt(req.query.page));
+    let limit = Math.max(1, parseInt(req.query.limit));
+    page = !isNaN(page) ? page : 1;
+    limit = !isNaN(limit) ? limit : 10;
+    const maxPost = 10;
+    let hiddenPost = page === 1 ? 0 : (page - 1) * limit;
+    let wargamePost = await Wargame.find({})
+      .sort('-createdAt') //내림차순 정렬
+      .skip(hiddenPost)
+      .limit(limit);
+    const totalPost = await Wargame.countDocuments({});
+    if (!totalPost) {
+      throw Error();
+    }
+    const totalPage = Math.ceil(totalPost / maxPost);
+    //페이징 구문 끝
 
-    res.render("wargame/index", { wargame });
+    res.render('wargame/index', {
+      posts: wargamePost,
+      paging: {
+        currentPage: page,
+        totalPage,
+        limit,
+      },
+    });
   } catch (error) {
     console.error(error);
   }
 };
 
 const getWargameCreatePage = (req, res) => {
-  res.render("wargame/create");
+  res.render('wargame/create');
 };
 
 const getWargameViewPage = async (req, res) => {
@@ -25,15 +48,38 @@ const getWargameViewPage = async (req, res) => {
 
   try {
     const wargame = await Wargame.findOne({ _id: id }).populate(
-      "userId",
-      "email nickname createdAt"
+      'userId',
+      'email nickname createdAt',
     );
 
-    const comment = await Comment.find({ wargameId: id })
-      .populate("userId", "email nickname")
-      .sort({ createdAt: "desc" });
+    //페이징 구문
+    let page = Math.max(1, parseInt(req.query.page));
+    let limit = Math.max(1, parseInt(req.query.limit));
+    page = !isNaN(page) ? page : 1;
+    limit = !isNaN(limit) ? limit : 3;
+    const maxComment = 3;
+    let hiddenComment = page === 1 ? 0 : (page - 1) * limit;
+    const comments = await Comment.find({ wargameId: id })
+      .populate('userId', 'email nickname')
+      .sort({ createdAt: 'desc' })
+      .skip(hiddenComment)
+      .limit(limit);
+    const totalComment = await Comment.countDocuments({ wargameId: id });
+    if (!totalComment) {
+      throw Error();
+    }
+    const totalPage = Math.ceil(totalComment / maxComment);
+    //페이징 구문 끝
 
-    res.render("wargame/view", { wargame, comment });
+    res.render('wargame/view', {
+      wargame,
+      comments,
+      paging: {
+        currentPage: page,
+        totalPage,
+        limit,
+      },
+    });
   } catch (error) {
     console.error(error);
   }
@@ -46,12 +92,12 @@ const postWargameUpdate = async (req, res) => {
   console.log(userId);
 
   try {
-    res.render("wargame/update");
+    res.render('wargame/update');
   } catch (error) {
     console.error(error);
   }
 
-  res.render("wargame/update");
+  res.render('wargame/update');
 };
 
 const postWargameCreate = async (req, res) => {
@@ -68,7 +114,7 @@ const postWargameCreate = async (req, res) => {
       userId: req.user,
     });
 
-    res.redirect("/wargame");
+    res.redirect('/wargame');
   } catch (error) {
     console.error(error);
   }
